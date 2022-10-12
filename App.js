ddd37ae978ac46fde983652318397fab478ec4e2
AoNoAdmiral <75779513+nhatlamairforce@users.noreply.github.com>
2022-10-12 19:32:27 +0700

    X
    A

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login.js'
 const Stack = createNativeStackNavigator();
 function HomeScreen() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="login" component={HomeScreen} />
+        <Stack.Screen name="login" component={Login} />
         <Stack.Screen name="main" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
